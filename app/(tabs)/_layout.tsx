785994f0eac394c9727ca141f48fd634207bd42c
Achmad Andy Dekanovy <achmdndy@gmail.com>
2025-09-02 16:18:50 +0700

    chore: add icons for bottom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,6 @@
+import { THEME } from "@/lib/theme";
 import { Tabs } from "expo-router";
+import { BookOpen, HandHeart, Home, Play, User,  } from "lucide-react-native";
 import { Platform } from "react-native";
 
 export default function TabLayout() {
@@ -7,43 +9,66 @@ export default function TabLayout() {
 			initialRouteName="home"
 			screenOptions={{
 				headerShown: false,
-				tabBarStyle: Platform.select({
+				tabBarLabelStyle: {
+					marginTop: 6,
+					fontSize: 12.
+				},
+				tabBarActiveTintColor: THEME.light.primary,
+				tabBarInactiveTintColor: THEME.light.mutedForeground,
+				tabBarStyle: [Platform.select({
 					ios: {
 						// Use a transparent background on iOS to show the blur effect
 						position: "absolute",
 					},
 					default: {},
-				}),
+				}), {
+					paddingTop: 4
+				}],
 			}}
 		>
 			<Tabs.Screen
 				name="home"
 				options={{
 					title: "Home",
+					tabBarIcon: ({color}) => (
+						<Home width={25} height={25} stroke={color}/>
+					)
 				}}
 			/>
 			<Tabs.Screen
 				name="quran"
 				options={{
 					title: "Quran",
+					tabBarIcon: ({color}) => (
+						<BookOpen width={25} height={25} stroke={color}/>
+					)
 				}}
 			/>
 			<Tabs.Screen
 				name="explore"
 				options={{
 					title: "Explore",
+					tabBarIcon: ({color}) => (
+						<Play width={25} height={25} stroke={color}/>
+					)
 				}}
 			/>
 			<Tabs.Screen
 				name="charity"
 				options={{
 					title: "Charity",
+					tabBarIcon: ({color}) => (
+						<HandHeart width={25} height={25} stroke={color}/>
+					)
 				}}
 			/>
 			<Tabs.Screen
 				name="profile"
 				options={{
 					title: "Profile",
+					tabBarIcon: ({color}) => (
+						<User width={25} height={25} stroke={color}/>
+					)
 				}}
 			/>
 		</Tabs>
